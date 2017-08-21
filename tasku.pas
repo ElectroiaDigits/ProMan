3170d34e448d84426e7e95a41eff06245127aea5
Baddin <baddin2016@gmail.com>
2017-08-21 22:59:23 +0100

    Adding tags to tasks

--- tasku.pas
+++ tasku.pas
@@ -18,12 +18,16 @@ type
     fDone: Boolean;
     fStartDate: TDateTime;
     fTitle: String;
+    fTags: String;
+				function getTags: String;
+				procedure setTags(AValue: String);
     procedure setTitle(AValue: String);
     public
       property Title: String read fTitle write setTitle;
       property StartDate: TDateTime read fStartDate ;
       property EndDate: TDateTime read fEndDate;
       property IsDone: Boolean read fDone;
+      property Tags: String read getTags write setTags;
       procedure Done;
 
   end;
@@ -35,6 +39,16 @@ implementation
 procedure TTask.setTitle(AValue: String);
 begin
   fTitle:= AValue;
+end;
+
+function TTask.getTags: String;
+begin
+    Result:= fTags;
+end;
+
+procedure TTask.setTags(AValue: String);
+begin
+    fTags:= AValue;
 end;
 
 
